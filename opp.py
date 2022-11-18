f22465f4f8c7214587f20f5516d40eea171801a6
satohitoshi1 <satohitoshi121@gmail.com>
2022-11-18 16:55:17 +0900

    add app git hitoshi

--- opp.py
+++ opp.py
@@ -7,7 +7,9 @@ face = cv2.imread("images/face_img.png")
 
 # eyeの透過処理その1
 eye[:, :, 3] = np.where(np.all(eye == 255, axis=-1), 0, 255)
-# cv2.imwrite("images/clear_eye.png", eye)
+cv2.imwrite("images/clear_eye.png", eye)
+
+clear_eye = cv2.imread("images/clear_eye.png")
 
 #  白色部分に対応するマスク画像を生成 透過処理その2 色が違う言われる
 # eye_mask = np.all(eye[:, :, :] == [255, 255, 255], axis=-1)
@@ -36,17 +38,15 @@ x_r = eyes[2][0]
 y_r = eyes[2][1]
 # faceの右目の大きさ
 w_r = eyes[2][2]
-h_r = eyes[2][3]
-
-# eye_dstをfaceの左目の大きさにあわせる
-resized_eye1 = cv2.resize(eye, dsize=(w_l, h_l))
-# eye_dstをfaceの左目の大きさにあわせる
-resized_eye2 = cv2.resize(eye, dsize=(w_r, h_r))
+h_r = eyes[2][3]  #
+# eyeをfaceの左目の大きさにあわせる
+resized_eye1 = cv2.resize(clear_eye, dsize=(w_l, h_l))
+# eyeをfaceの左目の大きさにあわせる
+resized_eye2 = cv2.resize(clear_eye, dsize=(w_r, h_r))
 # print(resized_eye.shape)
 # 28,28,3 確認
 
 # 座標を指定してfaceとeyeの画像を入れ替える
 face[y_l : h_l + y_l, x_l : w_l + x_l] = resized_eye1
 face[y_r : h_r + y_r, x_r : w_r + x_r] = resized_eye2
-
-cv2.imwrite("paste.jpg", face)
+cv2.imwrite("images/paste.jpg", face)  #
